516cf4fc895c1d14608760ade5bdc56030c33616
Seth Wheeler <wheeler.seth@gmail.com>
2020-02-28 15:49:09 -0700

    boards controller complete, index for board created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Board.create(title: "Project Destroy",)
+Board.create.lists.create(name:"To do", board_id: 1)
+Board.create.lists.create(name:"In Progress", board_id: 1)
+Board.create.lists.create(name:"Complete", board_id: 1)
+# List.create.tasks.create(name:"Task 1", description: "Blah", priority: 1, list_id: 1)
+# List.create.tasks.create(name:"Task 2", description: "Blah", priority: 2, list_id: 1)
+# List.create.tasks.create(name:"Task 3", description: "Blah", priority: 3, list_id: 1)
+# List.create.tasks.create(name:"Task 1", description: "Blah", priority: 1, list_id: 2)
+# List.create.tasks.create(name:"Task 2", description: "Blah", priority: 2, list_id: 2)
+# List.create.tasks.create(name:"Task 3", description: "Blah", priority: 3, list_id: 2)
+# List.create.tasks.create(name:"Task 1", description: "Blah", priority: 1, list_id: 3)
+# List.create.tasks.create(name:"Task 2", description: "Blah", priority: 2, list_id: 3)
+# List.create.tasks.create(name:"Task 3", description: "Blah", priority: 3, list_id: 3)
+
+puts "seeded"
+
+
